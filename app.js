4512d56c5745050131a809403bf75e8c49a40a7f
Raphael <raphael.ferreira@aspireiq.com>
2023-04-04 07:27:28 -0300

    fix mongo uri

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 const uri = process.env.MONGO_URI;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri);
 client.connect(async err => {
   client.close();
 });
